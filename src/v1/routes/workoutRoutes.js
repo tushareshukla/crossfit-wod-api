5793f9c8417ae341106beb93787ecea3abdd8687
pashupat <tushareshukla@gmail.com>
2024-03-17 11:27:33 +0530

    workout.routes.js update

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,1 +1,21 @@
-const express = 
+const express = require("express");
+const router = express.Router();
+
+router.get('/', (req, res)=>{
+    res.send("Get all wrokouts")
+})
+
+router.get('/:workoutId',(req,res)=>{
+    res.send("Get an existing workout");
+})
+
+router.post('/',(req,res)=>{
+    res.send("Create a new workout");
+})
+router.patch("/:workoutID",(req,res)=>{
+    res.send("Update an existing workout")
+})
+router.delete('/:workoutID', (req,res)=>{
+    res.send("Delete an existing workout")
+})
+module.exports = router;
